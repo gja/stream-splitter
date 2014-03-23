ce5a24da4a91a8c2f3910a07e90fa6e401abd4ff
Tejas Dinkar <tejas@gja.in>
2014-03-23 20:55:18 +0530

    Readabilty commit

--- StreamSplitter.java
+++ StreamSplitter.java
@@ -22,13 +22,13 @@ class StreamSplitterEmptyReader extends Reader {
     public void close() { }
 }
 
-class InputBlock {
+class InputPage {
     private char[] buffer;
     private int length;
 
-    InputBlock(Reader reader, int blockSize) throws IOException {
-        buffer = new char[blockSize];
-        length = reader.read(buffer, 0, blockSize);
+    InputPage(Reader reader, int pageSize) throws IOException {
+        buffer = new char[pageSize];
+        length = reader.read(buffer, 0, pageSize);
     }
 
     Reader toReader() throws IOException {
@@ -41,17 +41,21 @@ class InputBlock {
 
 public class StreamSplitter {
     private List<StreamSplitterStream> streams;
-    private Map<Integer, InputBlock> buffers;
-    private int blockSize;
+    private Map<Integer, InputPage> buffers;
+    private int pageSize;
     private Reader input;
-    private int blockCount;
+    private int pageCount;
 
-    public StreamSplitter(Reader input, int blockSize, int blockCount) {
+    public StreamSplitter(Reader input, int pageSize, int pageCount) {
         streams = new ArrayList<StreamSplitterStream>();
-        buffers = new HashMap<Integer, InputBlock>();
-        this.blockSize = blockSize;
+        buffers = new HashMap<Integer, InputPage>();
+        this.pageSize = pageSize;
         this.input = input;
-        this.blockCount = blockCount;
+        this.pageCount = pageCount;
+    }
+
+    public StreamSplitter(Reader input) {
+        this(input, 262144, 4);
     }
 
     public synchronized Reader getStream() {
@@ -60,57 +64,65 @@ public class StreamSplitter {
         return buffer;
     }
 
-    Reader fetchBlock(int blockNumber) throws IOException {
-        InputBlock block = buffers.get(blockNumber);
-        if (block == null)
-            block = populateBlock(blockNumber);
+    Reader fetchPage(int pageNumber) throws IOException {
+        InputPage page = buffers.get(pageNumber);
+        if (page == null)
+            page = populatePage(pageNumber);
 
         wakeUpSleepingThreads();
 
-        return block.toReader();
+        return page.toReader();
     }
 
     void deleteStream(Reader reader) {
         streams.remove(reader);
     }
 
-    private synchronized void deleteBlock(Integer blockNumber) throws IOException {
-        try {
-            while(isBlockInUse(blockNumber))
-                wait();
+    private void deletePage(Integer pageNumber) throws IOException {
+        synchronized(streams) {
+            try {
+                while(isPageInUse(pageNumber))
+                    streams.wait();
+            } catch (InterruptedException e) {
+                throw new IOException("StreamSplitter: Interupted while waiting for readers to catch up", e);
+            }
+        }
 
-            InputBlock blockToDelete = buffers.get(blockNumber);
-            if (blockToDelete == null)
-                return;
-            buffers.remove(blockNumber);
-        } catch (InterruptedException e) {
-            throw new IOException("StreamSplitter: Interupted while waiting for readers to catch up", e);
+
+        InputPage pageToDelete = buffers.get(pageNumber);
+        if (pageToDelete == null)
+            return;
+
+        synchronized(buffers) {
+            buffers.remove(pageNumber);
         }
     }
 
-    private synchronized InputBlock populateBlock(Integer blockNumber) throws IOException {
-        // Someone else has populated this while we waited
-        if (buffers.get(blockNumber) != null)
-            return buffers.get(blockNumber);
+    private InputPage populatePage(Integer pageNumber) throws IOException {
+        synchronized(buffers) {
+            // Someone else has populated this while we waited
+            if (buffers.get(pageNumber) != null)
+                return buffers.get(pageNumber);
 
-        // First delete the old block
-        deleteBlock(blockNumber - blockCount);
+            deletePage(pageNumber - pageCount);
 
-        InputBlock block = new InputBlock(input, blockSize);
-        buffers.put(blockNumber, block);
-        return block;
+            InputPage page = new InputPage(input, pageSize);
+            buffers.put(pageNumber, page);
+            return page;
+        }
     }
 
-    private synchronized boolean isBlockInUse(int blockNumber) {
+    private boolean isPageInUse(int pageNumber) {
         for (StreamSplitterStream stream : streams)
-            if (stream.blockNumber == blockNumber)
+            if (stream.pageNumber == pageNumber)
                 return true;
-
         return false;
     }
 
-    private synchronized void wakeUpSleepingThreads() {
-        notify();
+    private void wakeUpSleepingThreads() {
+        synchronized(streams) {
+            streams.notify();
+        }
     }
 }
 
@@ -118,29 +130,29 @@ public class StreamSplitter {
 
 class StreamSplitterStream extends Reader {
     private StreamSplitter parent;
-    private Reader readable;
-    int blockNumber;
+    private Reader reader;
+    int pageNumber;
 
     StreamSplitterStream(StreamSplitter parent) {
         this.parent = parent;
-        this.readable = new StreamSplitterEmptyReader();
-        blockNumber = -1;
+        this.reader = new StreamSplitterEmptyReader();
+        pageNumber = -1;
     }
 
     public synchronized int read(char[] cbuf, int off, int len) throws IOException {
-        int ret = readable.read(cbuf, off, len);
+        int ret = reader.read(cbuf, off, len);
 
         if (ret != -1)
             return ret;
 
-        readable.close();
-        readable = parent.fetchBlock(++blockNumber);
-        return readable.read(cbuf, off, len);
+        reader.close();
+        reader = parent.fetchPage(++pageNumber);
+        return reader.read(cbuf, off, len);
     }
 
     public synchronized void close() throws IOException {
-        readable.close();
-        readable = new StreamSplitterEmptyReader();
+        reader.close();
+        reader = new StreamSplitterEmptyReader();
         parent.deleteStream(this);
     }
 }
